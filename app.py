f19fda8cbd9aa84139c4bbc835a71e399d307536
HizRadit07 <63536655+HizRadit07@users.noreply.github.com>
2020-08-04 13:54:23 +0700

    update on hello function

--- app.py
+++ app.py
@@ -45,9 +45,10 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="hello user\nchatbot here"))
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="is there anything you want me to do?"))
+        if 'bonjour' in msg:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="is there anything you want me to do?"))
     elif 'apa kabar' in msg:
         line_bot_api.reply_message(
             event.reply_token, 
