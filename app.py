518f1d0f8f7cf465d6603eb9799525e0efb5cfe7
HizRadit07 <63536655+HizRadit07@users.noreply.github.com>
2020-08-05 17:53:12 +0700

    removed the number options
    apparently line processes each of the number so say this text "asdfqwerfas1" it contains a 1, it will output GKUB option, currently have no idea how to fix

--- app.py
+++ app.py
@@ -55,48 +55,48 @@ def handle_message(event):
             TextSendMessage(text="test ready, greetings Dαybreak-"))
     #easter egg functions, lol
     #dalam ITB messages
-    elif ('1' in msg) or ('GKUB' in msg) or ('gkub' in msg) or ('Gkub' in msg):
+    elif ('GKUB' in msg) or ('gkub' in msg) or ('Gkub' in msg):
         line_bot_api.reply_message(
             event.reply_token, 
             TextSendMessage(text="List tempat fotokopi di GKUB\n\n1. GKUB LT 1\nJUAL LENGKAP\njam operasional: 9.00-16.00\n\n2. GKUB LT 1\nJUAL LENGKAP\nJam operasional: 09.00-17.00"))
-    elif ('2' in msg) or ('Labtek 5' in msg) or ('Labtek V' in msg) or ('LABTEK 5' in msg) or ('LABTEK V' in msg):
+    elif ('Labtek 5' in msg) or ('Labtek V' in msg) or ('LABTEK 5' in msg) or ('LABTEK V' in msg):
         line_bot_api.reply_message(
             event.reply_token, 
             TextSendMessage(text="List tempat fotokopi di Labtek V\n\n1. LABTEK V LT 1 (DEKAT SEKRE HMIF 1)\nHANYA FOTOKOPI PRINT\nJam operasional: 09.00-15.00"))
-    elif ('3' in msg) or ('GKUT' in msg) or ('Gkut' in msg) or ('gkut' in msg):
+    elif ('GKUT' in msg) or ('Gkut' in msg) or ('gkut' in msg):
         line_bot_api.reply_message(
             event.reply_token, 
             TextSendMessage(text="List tempat fotokopi di GKUT\n\n1. GKUT LT 1\nJUAL LENGKAP\nJam operasional: 09.00 - 15.00"))
-    elif ('4' in msg) or ('Labtek 8' in msg) or ('Labtek VIII' in msg) or ('LABTEK 8' in msg) or ('LABTEK VIII' in msg):
+    elif ('Labtek 8' in msg) or ('Labtek VIII' in msg) or ('LABTEK 8' in msg) or ('LABTEK VIII' in msg):
         line_bot_api.reply_message(
             event.reply_token, 
             TextSendMessage(text="List tempat fotokopi di LABTEK VIII\n\n1. LABTEK VIII LT BASEMENT\nHANYA FOTOKOPI, PRINT\nJam operasional: 09.00-15.00"))
-    elif ('5' in msg) or ('Labtek biru' in msg) or ('labtek biru' in msg):
+    elif ('Labtek biru' in msg) or ('labtek biru' in msg):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di Labtek biru\n\n1. LABTEK BIRU LT 1\nJUAL LENGKAP\nJam operasional: 09.00-16.00"))
-    elif ('6' in msg) or ('sbm' in msg) or ('SBM' in msg) or ('Sbm' in msg):
+    elif ('sbm' in msg) or ('SBM' in msg) or ('Sbm' in msg):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di SBM\n\n1. SBM LT 2\nJUAL LENGKAP\nJam operasional: 09.00-15.00"))
-    elif ('7' in msg) or ('ITB Press' in msg) or ('Itb press' in msg) or ('itb press' in msg):
+    elif ('ITB Press' in msg) or ('Itb press' in msg) or ('itb press' in msg):
          line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di ITB Press\n\n1. ITB Press (Di belakang perpustakaan)\nHanya Fotokopi\nJam operasional: 09.00-17.00\nLink google maps: https://goo.gl/maps/mVAZPxmtv5sXtdGF7"))
-    elif ('8' in msg) or ('PAU' in msg) or ('pau' in msg) or ('Pau' in msg) or ('Gedung PAU' in msg) or ('gedung PAU' in msg) or ('Gedung pau' in msg) or ('gedung pau' in msg):
+    elif ('PAU' in msg) or ('pau' in msg) or ('Pau' in msg) or ('Gedung PAU' in msg) or ('gedung PAU' in msg) or ('Gedung pau' in msg) or ('gedung pau' in msg):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di Gedung PAU\n\n1. GEDUNG PAU LT 1 (BAGIAN DEPAN)\nHANYA FOTOKOPI,PRINT\nJam operasional: 09.00-15.00"))
     #luar ITB messages
-    elif ('9' in msg) or ('cisitu' in msg) or ('Cisitu' in msg):
+    elif ('cisitu' in msg) or ('Cisitu' in msg):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di daerah Cisitu\n\n1.	Nama tempat fotokopi	: Alifia Fotocopy & Digital Printing\nNama Jalan		: Jalan Cisitu Lama No 42 Dago, Kecamatan Coblong,Bandung,Jawa Barat\nYang dijual		: Jasa Fotocopy,printing, ATK, laminating,jilid\nJam operasi		: \nSenin - Sabtu : 07.00 – 22.00\nMinggu : 10.00 – 22.00\nLink Gmaps		: https://goo.gl/maps/pJFHVczpXbnNsEYp6\n\n2. 2.    Nama tempat fotokopi	: Putri Copy Centre\nNama Jalan		: Jl. Cisitu Lama No.32, Dago, Kecamatan Coblong, Kota Bandung, Jawa Barat 40135\nYang dijual		: ATK,Jasa printing,fotocopy,Jilid,Laminating,Scaning,Cetak Foto\nJam operasi		: \nSenin - Sabtu : 07.00 – 22.00\nMinggu : 10.00 – 21.00\nLink Gmaps		: https://goo.gl/maps/GpM5iWL5PKBCw43E6\n\n3.   Nama tempat fotokopi	: Edo Copy Centre\nNama Jalan		: Jl. Cisitu Lama No.37, Dago, Kecamatan Coblong, Kota Bandung, Jawa Barat 40135\nYang dijual		: ATK, Print Foto, Print dan Fotocopy,Laminating,Jilid\nJam operasi		: \nSenin - Sabtu : 07.00 – 22.00\nMinggu : 10.00 – 22.00\nLink Gmaps		: https://goo.gl/maps/eqiZK3duCGGR5QoNA"))
-    elif ('10' in msg) or ('DU' in msg) or ('Du' in msg) or ('du' in msg) or ('Dipatiukur' in msg) or ('dipatiukur' in msg):
+    elif ('DU' in msg) or ('Du' in msg) or ('du' in msg) or ('Dipatiukur' in msg) or ('dipatiukur' in msg):
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="List tempat fotokopi di daerah Dipatiukur (DU)\n\n1.  1.	Nama tempat fotokopi	: Jaya Abadi Photocopy\nNama jalan		: Jl. Dipati Ukur No.269, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Jasa fotokopi, mencetak dokumen, ATK, hard cover, soft cover, jilid ring, laminating\nJam operasi		: \nSenin s.d Kamis buka 24 jam\nJumat 24.00 – 11.00 dan 13.00 – 23.59 \nSabtu 12.00 – 19.00\nMinggu 08.00 – 12.00\nLink google maps	: https://goo.gl/maps/YW4JZ8sSnAP6KVY87\n\n2.   Nama tempat fotokopi	: Cahaya Abadi Fotocopy, Penjilidan & Digital Printing\nNama jalan		: Jl. Dipati Ukur No.122D, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Print, scan, fotokopi, penjilidan, dll.\nJam operasi		: Senin s.d Minggu buka 24 jam\nLink google maps	: https://goo.gl/maps/yPuU8ZZwNKDJSXnVA\n\n3.	Nama tempat fotokopi	: Gugum Offset\nNama jalan		: Jl. Dipati Ukur No.42 /60, Lebakgede, Kecamatan Coblong, Bandung, Jawa Barat 40132\nYang dijual		: Print, fotokopi, penjilidan, dll.\nJam operasi		: Senin s.d Minggu pukul 08.00 – 21.00\nLink google maps	: https://goo.gl/maps/NfagCS1RsKRc9mn6A"))
-    elif ('11' in msg) or ('Tubis' in msg) or ('tubis' in msg) or ('Tubagus Ismail' in msg) or ('tubagus ismail' in msg) or ('Tubagus ismail' in msg):
+            TextSendMessage(text="List tempat fotokopi di daerah Dipatiukur (DU)\n\n1. Nama tempat fotokopi	: Jaya Abadi Photocopy\nNama jalan		: Jl. Dipati Ukur No.269, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Jasa fotokopi, mencetak dokumen, ATK, hard cover, soft cover, jilid ring, laminating\nJam operasi		: \nSenin s.d Kamis buka 24 jam\nJumat 24.00 – 11.00 dan 13.00 – 23.59 \nSabtu 12.00 – 19.00\nMinggu 08.00 – 12.00\nLink google maps	: https://goo.gl/maps/YW4JZ8sSnAP6KVY87\n\n2.   Nama tempat fotokopi	: Cahaya Abadi Fotocopy, Penjilidan & Digital Printing\nNama jalan		: Jl. Dipati Ukur No.122D, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Print, scan, fotokopi, penjilidan, dll.\nJam operasi		: Senin s.d Minggu buka 24 jam\nLink google maps	: https://goo.gl/maps/yPuU8ZZwNKDJSXnVA\n\n3.	Nama tempat fotokopi	: Gugum Offset\nNama jalan		: Jl. Dipati Ukur No.42 /60, Lebakgede, Kecamatan Coblong, Bandung, Jawa Barat 40132\nYang dijual		: Print, fotokopi, penjilidan, dll.\nJam operasi		: Senin s.d Minggu pukul 08.00 – 21.00\nLink google maps	: https://goo.gl/maps/NfagCS1RsKRc9mn6A"))
+    elif ('Tubis' in msg) or ('tubis' in msg) or ('Tubagus Ismail' in msg) or ('tubagus ismail' in msg) or ('Tubagus ismail' in msg):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="List tempat fotokopi di daerah Tubagus Ismail (Tubis)\n\n1.   Nama tempat fotokopi	: Rindu Dago Digital Printing & Copier\nAlamat			: Jl. Tubagus Ismail Raya No.1, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Jasa fotokopi, print foto,print laser, ID Card, ATK, hard cover, soft cover, jilid ring, laminating\nJam operasi		: 24 Jam\nLink google maps	: https://www.google.com/maps/dir//fotokopi+tubagus+ismail/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e68e6f924ae1f07:0x2702d20bd94addf3?sa=X&ved=2ahUKEwiewJ7dgv7qAhUQU30KHT_5CW8Q9RcwAHoECAYQCg\n\n2.	Nama tempat fotokopi	: Batara\nAlamat			: Jl. Tubagus Ismail Bawah, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132\nYang dijual		: Jasa fotokopi, print foto, ATK, jilid ring, laminating\nJam operasi		: 08.00-21.00 WIB\nLink google maps	: https://www.google.com/maps/dir//fotokopi+di+tubagus+ismail/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x2e68e6544cbae1a1:0xb3260f8914f18543?sa=X&ved=2ahUKEwjT6omug_7qAhWLSH0KHQmhBooQ9RcwAHoECAUQCA\n\n3.	Nama tempat fotokopi	: Salsabila - 99\nAlamat			: Jl. Tubagus Ismail Raya No.44, Sekeloa, Kecamatan Coblong, Kota Bandung, Jawa Barat 40134\nYang dijual		: Jasa fotokopi, print foto, ATK, jilid ring, laminating\nJam operasi		: 08.00-20.00 WIB\nLink google maps	: https://www.google.com/maps/dir/-6.2469977,106.8532384/Salsabila+-+99,+Jl.+Tubagus+Ismail+Raya+No.44,+Sekeloa,+Kecamatan+Coblong,+Kota+Bandung,+Jawa+Barat+40134/@-6.5219234,106.6492494,9z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x2e68e6ffa53ba211:0x295e62cb6d62dcaa!2m2!1d107.6192492!2d-6.8848563"))
